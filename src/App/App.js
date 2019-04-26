65595c669e77f795d0e24181f1613dfdfeb84670
Iryna <uaireneua@gmail.com>
2019-04-26 21:59:44 +0300

    add Main component and style, connect to App.js

--- src/App/App.js
+++ src/App/App.js
@@ -4,11 +4,13 @@ import './../common/style/reset.css';
 import './../common/style/base.css';
 
 import Header from './Header/Header';
+import Main from './Main/Main';
 
 const App =() => {
 	return (
 	<div>
 		<Header/>
+		<Main/>
 	</div>
 	);
 }
---
+++ src/App/Main/Main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './main.css';
+
+const Main = () => {
+    return (
+        <main>
+            <div className="bg-main">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-m-lg-9">
+                            Hello
+                        </div>
+                        <div className="col-m-lg-3">
+                            Hello
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default Main;
